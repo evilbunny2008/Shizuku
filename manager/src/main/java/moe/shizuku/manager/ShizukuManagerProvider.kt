90389e6458588f0823a73733ff2977941f43b306
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-09-12 01:00:22 +1000

    flush

--- manager/src/main/java/moe/shizuku/manager/ShizukuManagerProvider.kt
+++ manager/src/main/java/moe/shizuku/manager/ShizukuManagerProvider.kt
@@ -1,14 +1,12 @@
 package moe.shizuku.manager
 
 import android.os.Bundle
-import androidx.core.os.bundleOf
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.android.asCoroutineDispatcher
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
 import moe.shizuku.api.BinderContainer
 import moe.shizuku.manager.utils.Logger.LOGGER
 import moe.shizuku.manager.utils.ShizukuStateMachine
@@ -16,6 +14,7 @@ import rikka.shizuku.Shizuku
 import rikka.shizuku.ShizukuApiConstants.USER_SERVICE_ARG_TOKEN
 import rikka.shizuku.ShizukuProvider
 import rikka.shizuku.server.ktx.workerHandler
+import kotlin.time.Duration.Companion.milliseconds
 
 class ShizukuManagerProvider : ShizukuProvider() {
 
@@ -37,17 +36,18 @@ class ShizukuManagerProvider : ShizukuProvider() {
                 extras.classLoader = BinderContainer::class.java.classLoader
 
                 val token = extras.getString(USER_SERVICE_ARG_TOKEN) ?: return null
-                val binder = extras.getParcelable<BinderContainer>(EXTRA_BINDER)?.binder ?: return null
+                //val binder = BundleCompat.getParcelable(extras, EXTRA_BINDER, BinderContainer::class.java)?.binder ?: return null
 
                 return runBlocking {
                     try {
-                        withTimeout(5000) {
+                        withTimeout(5000.milliseconds) {
                             ShizukuStateMachine.asFlow().first { it == ShizukuStateMachine.State.RUNNING }
                             withContext(workerHandler.asCoroutineDispatcher()) {
                                 try {
                                     val reply = Bundle()
-                                    Shizuku.attachUserService(binder, bundleOf(USER_SERVICE_ARG_TOKEN to token))
-                                    reply!!.putParcelable(EXTRA_BINDER, BinderContainer(Shizuku.getBinder()))
+                                    //Shizuku.attachUserService(binder, bundleOf(USER_SERVICE_ARG_TOKEN to token))
+                                    Bundle().apply { putString(USER_SERVICE_ARG_TOKEN, token) }
+                                    reply.putParcelable(EXTRA_BINDER, BinderContainer(Shizuku.getBinder()))
                                     reply
                                 } catch (e: Throwable) {
                                     LOGGER.e(e, "attachUserService $token")
